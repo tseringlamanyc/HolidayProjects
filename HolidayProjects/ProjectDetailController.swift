c3d787893bdfaf64e76a5c81c7e242753fae6b7f
Tsering Lama <tseringlama@pursuit.org>
2020-01-16 23:41:36 -0500

    set up UI and connected all the outlets the first VC populates with table view

--- HolidayProjects/ProjectDetailController.swift
+++ HolidayProjects/ProjectDetailController.swift
@@ -10,50 +10,50 @@ import UIKit
 import ImageKit
 
 class ProjectDetailController: UIViewController {
-  
-  @IBOutlet weak var firstNameLabel: UILabel!
-  @IBOutlet weak var lastNameLabel: UILabel!
-  @IBOutlet weak var githubButton: UIButton!
-  @IBOutlet weak var projectImageView: UIImageView!
-  @IBOutlet weak var descriiptionLabel: UILabel!
-  
-  var project: Project? {
-    didSet {
-      DispatchQueue.main.async {
-        self.updateUI()
-      }
-    }
-  }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  private func updateUI() {
-    guard let project = project else {
-      return
-    }
-    navigationItem.title = project.projectName
     
-    // TODO: uncomment below when all outlets are connected to the ScrollView
-    /*
-    firstNameLabel.text = project.firstName
-    lastNameLabel.text = project.lastName
-    descriiptionLabel.text = project.description
-    projectImageView.getImage(with: project.imageURL) { [weak self] (result) in
-      switch result {
-      case .failure(let appError):
-        print("error fetching image \(appError)")
-      case .success(let image):
-        DispatchQueue.main.async {
-          self?.projectImageView.image = image
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var githubButton: UIButton!
+    @IBOutlet weak var projectImageView: UIImageView!
+    @IBOutlet weak var descriiptionLabel: UILabel!
+    
+    var project: Project? {
+        didSet {
+            DispatchQueue.main.async {
+                self.updateUI()
+            }
         }
-      }
     }
-    */
-  }
-  
-  // TODO: add an action to open up a given Github page via SFSafariViewController
-  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func updateUI() {
+        guard let project = project else {
+            return
+        }
+        navigationItem.title = project.projectName
+        
+        // TODO: uncomment below when all outlets are connected to the ScrollView
+        
+        firstNameLabel.text = project.firstName
+        lastNameLabel.text = project.lastName
+        descriiptionLabel.text = project.description
+        projectImageView.getImage(with: project.imageURL) { [weak self] (result) in
+            switch result {
+            case .failure(let appError):
+                print("error fetching image \(appError)")
+            case .success(let image):
+                DispatchQueue.main.async {
+                    self?.projectImageView.image = image
+                }
+            }
+        }
+        
+    }
+    
+    // TODO: add an action to open up a given Github page via SFSafariViewController
+    
 }
 
